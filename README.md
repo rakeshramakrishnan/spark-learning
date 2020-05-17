ee22923accf3a20467e39712ffd298387c6a3f66
rakeshramakrishnan92 <rakeshramakrishnan92@gmail.com>
2020-05-17 16:08:11 +0530

    First spark job!  - with subbu

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# spark-learning
-Spark learning
+# Intro
+Assuming that local kubernetes is running, along with tiller
+
+# Setup spark operator
+```
+$ helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+$ helm install --name spark incubator/sparkoperator --set sparkJobNamespace=default
+```
+
+# Build local project into docker image
+```
+$ ./gradlew build
+$ docker build -t=sample-spark-job-image .
+```
+
+# Deploy docker image onto k8
+```
+kubectl apply -f spark-operator-submit.yaml
+```
+
+# Monitor application via UI of driver
+```
+kubectl port-forward service/spark-pi-custom-ui-svc 4040:4040
+```
+Note that name of application = spark-pi-custom in above example. It is set under `metadata.name` under `spark-operator-submit.yaml`
+
+# Monitor application by describing SparkApplication
+```
+kubectl describe sparkapplication spark-pi-custom
+```
+
+# Delete the spark application
+```
+kubectl delete sparkapplication spark-pi-custom
+```
---
+++ src/main/java/spark/examples/computation/PiEstimation.java
@@ -0,0 +1,41 @@
+package spark.examples.computation;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PiEstimation {
+  public static void main(String[] args) throws Exception {
+    SparkSession spark = SparkSession
+      .builder()
+      .appName("PiEstimation")
+      .getOrCreate();
+
+    JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+    int slices = 4;
+    int n = 100000 * slices;
+    List<Integer> l = new ArrayList<>(n);
+    for (int i = 0; i < n; i++) {
+      l.add(i);
+    }
+
+    JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+    int count = dataSet.map(integer -> {
+      double x = Math.random() * 2 - 1;
+      double y = Math.random() * 2 - 1;
+      return (x * x + y * y <= 1) ? 1 : 0;
+    }).reduce((integer, integer2) -> integer + integer2);
+
+    System.out.println("Computed Pi!!! It is: " + 4.0 * count / n);
+    System.out.println("About to sleep");
+
+    Thread.sleep(300000);
+    System.out.println("Sleep done");
+    spark.stop();
+  }
+}
