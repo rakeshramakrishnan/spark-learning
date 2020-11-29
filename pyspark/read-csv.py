b885605f426858321a38bc3e136b030913a39c50
rakeshramakrishnan92 <rakeshramakrishnan92@gmail.com>
2020-11-29 22:30:00 +0530

    Try out spark sql

--- pyspark/read-csv.py
+++ pyspark/read-csv.py
@@ -1,4 +1,5 @@
 import pyspark
+import time
 
 spark = pyspark.sql.SparkSession.builder.appName("read-csv-1").getOrCreate()
 
@@ -8,8 +9,30 @@ flightData = spark\
     .option("header", "true") \
     .csv("/Users/rakeshramakrishnan/Personal/Learning/Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv")
 
-print(flightData.take(3))
+# print(flightData.take(3))
+#
+# flightData\
+#     .sort("count")\
+#     .explain()
 
-flightData\
-    .sort("count")\
-    .explain()
+# spark SQL
+# flightData.createOrReplaceTempView("flight_data")
+#
+# maxCount = spark.sql("""
+# SELECT DEST_COUNTRY_NAME, sum(count) as destination_total FROM flight_data
+# GROUP BY DEST_COUNTRY_NAME
+# ORDER BY sum(count) DESC
+# LIMIT 5
+# """)
+#
+# maxCount.show()
+
+flightData \
+    .groupBy("DEST_COUNTRY_NAME") \
+    .sum("count") \
+    .withColumnRenamed("sum(count)", "destination_total")\
+    .sort("destination_total", ascending=False) \
+    .limit(5) \
+    .show()
+
+time.sleep(100)
