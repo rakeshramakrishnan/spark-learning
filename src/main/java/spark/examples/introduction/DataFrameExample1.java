3df8130270a2f78b9be51d9c6b8cbd9f03c74f9a
rakeshramakrishnan92 <rakeshramakrishnan92@gmail.com>
2020-11-29 22:59:11 +0530

    Try out spark sql in java

--- src/main/java/spark/examples/introduction/DataFrameExample1.java
+++ src/main/java/spark/examples/introduction/DataFrameExample1.java
@@ -1,21 +1,28 @@
 package spark.examples.introduction;
 
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
-
-import java.util.List;
+import org.apache.spark.sql.functions;
 
 public class DataFrameExample1 {
-  public static void main() {
+  public static void main(String[] args) {
     SparkSession spark = SparkSession
       .builder()
       .appName("example-1")
       .getOrCreate();
 
-    List df = spark
-      .range(100)
-      .toDF()
-      .collectAsList();
+    Dataset flightData = spark
+      .read()
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv("/Users/rakeshramakrishnan/Personal/Learning/Spark-The-Definitive-Guide/data/flight-data/csv/2015-summary.csv");
 
-    System.out.println(String.format("Hello world", df.toString()));
+    flightData
+    .groupBy("DEST_COUNTRY_NAME")
+    .sum("count")
+    .withColumnRenamed("sum(count)", "destination_total")
+    .sort(functions.desc("destination_total"))
+    .limit(5)
+    .show();
   }
 }
